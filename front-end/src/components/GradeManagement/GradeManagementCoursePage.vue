1cd3625740ab507d0cd1349b6dac362e2618db02
zf909 <80331399+zf909@users.noreply.github.com>
2021-12-27 01:27:43 +0800

    Add files via upload

--- front-end/src/components/GradeManagement/GradeManagementCoursePage.vue
+++ front-end/src/components/GradeManagement/GradeManagementCoursePage.vue
@@ -4,33 +4,122 @@
         <va-breadcrumbs separator=">">
             <va-breadcrumbs-item label="成绩管理" disabled />
             <va-breadcrumbs-item label="所有课程" to="/home/grademanagement" />
-        <va-breadcrumbs-item label="所有实验项目" to="/home/grademanagement/course" />
+        <va-breadcrumbs-item label="课程成绩管理" to="/home/grademanagement/course" />
         </va-breadcrumbs>
       </div>
     <va-card>
-      <va-card-title style="font-size: 20px">所有课程项目</va-card-title>
+      <va-card-title style="font-size: 20px">课程成绩管理</va-card-title>
         <va-card-content v-show="tasks.length">
-            <va-card 
+          <div>
+          <div style="display: flex">
+              <a-button style="margin-right: 10px" @click="this.Scorestatus=1">课程项目成绩</a-button>
+              <a-button @click="this.Scorestatus=2">课程成员成绩</a-button>
+          </div>
+          <div style="height: 12px" />
+          </div>
+            <!-- <va-card 
             v-for="(task, index) in tasks"
             :key="index"
             color="#b5c4b1" 
             gradient
             style="margin-bottom: 10px"
             >
-              <va-card-content style="rgb(60, 60, 60); font-weight: bold">
+              <va-card-content v-if="this.Scorestatus==1" style="rgb(60, 60, 60); font-weight: bold">
                 <div style="display: flex">
-                  <!-- va-card高度: 76px -->
-                  <!-- va-card高度: 36px -->
+                
                   <div style="line-height: 36px; width: 90%; font-size: 18px">{{task.name}}</div>
                   <div><va-button @click="switchToTask(task.id, index)" color="#e0e5df" style="color: rgb(40,40,40)">点击进入</va-button></div>
                 </div>
               </va-card-content>
-            </va-card>
+            </va-card> -->
+          <el-table v-if="this.Scorestatus==1"
+          :data="tasks.filter(
+              (data) =>
+                !search || data.name.toLowerCase().includes(search.toLowerCase())
+            )
+          "
+          style="width: 100%"
+          >
+           
+            <el-table-column label="项目名称" prop="name" />
+            <el-table-column align="right">
+            <template #header>
+              <el-input v-model="search" size="mini" placeholder="Type to search" />
+            </template>
+            <template #default="scope">
+              
+              <el-button
+                size="mini"
+                type="danger"
+                @click="switchToTask(scope.row)"
+                >点击进入</el-button
+              >
+              </template>
+              </el-table-column>
+          </el-table>
+
+          <el-table v-if="this.Scorestatus==2"
+          stripe
+          :data="student.filter(
+              (data) =>
+                !search || data.name.toLowerCase().includes(search.toLowerCase())
+            )
+          "
+          style="width: 100%"
+          >
+            <el-table-column label="学号" prop="id" />
+            <el-table-column label="姓名" prop="name" />
+            <el-table-column align="right">
+            <template #header>
+              <el-input v-model="search" size="mini" placeholder="Type to search" />
+            </template>
+            <template #default="scope">
+              
+              <a-button style="margin-left: 10px" @click="handleMark(scope.row)">评分</a-button>
+              </template>
+              </el-table-column>
+          </el-table>
         </va-card-content>
 
         <va-card-content v-show="!tasks.length">暂无课程项目</va-card-content>
     </va-card>
   </div>
+
+  <a-modal
+    v-model:visible="markModalVisible">
+    <template #title>
+      评分
+    </template>
+    <div style="text-align: center">
+      <div style="margin-bottom: 10px">分数</div>
+      <a-input-number style="width: 15%" v-model="markedStudentScore" :min="0" :max="100" />
+      <a-button style="margin-left: 10px" @click="updateScore">提交</a-button>
+      <h3 style="text-align:left">项目得分(90%)</h3>
+      <el-table 
+      :data="score.filter(
+          (data) =>
+            !search || data.name.toLowerCase().includes(search.toLowerCase())
+        )
+      "
+      style="width: 100%"
+      >
+        <el-table-column label="项目名称" prop="3" />
+        <el-table-column label="得分" prop="4" />
+        <el-table-column align="right">
+        <template #header>
+          <el-input v-model="search" size="mini" placeholder="Type to search" />
+        </template>
+        <template #default="scope">
+          
+          <a-button style="margin-left: 10px" @click="handleMark(scope.row)">评分</a-button>
+          </template>
+          </el-table-column>
+      </el-table>
+      <div style="height: 20px" />
+      <h3 style="text-align:left">签到次数(10%)</h3>
+      <div>本学期已签到{{this.count}}次</div>
+    </div>
+    </a-modal>
 </template>
 
 <script>
@@ -40,8 +129,15 @@ export default {
       teacherId: '',
       courseId: '',
       courseName: '',
-
+      Scorestatus:1,
       tasks: [],
+      student:[],
+      search:'',
+      markModalVisible:false,
+      studentid:'',
+      markedStudentScore:'',
+      score:[],
+      count:''
     }
   },
   mounted() {
@@ -60,22 +156,108 @@ export default {
         this.tasks = res
       })
     })
+    fetch(this.$URL + "/takes/get/course/detail?courseId=" + this.courseId, {
+      method: "GET",
+      headers: { "satoken": localStorage.getItem("token") }
+    }).then(response => {
+      console.log(response)
+      let result = response.json()
+      result.then(res => {
+        console.log(res)
+        this.student = res
+        
+      })
+    })
   },
   methods: {
-    switchToTask (taskId, index) {
+    handleMark(row) {
+       fetch(this.$URL + "/finishes/get/record/detail/studentAndCourse?courseId=" + this.courseId+"&studentId="+row.id, {
+        method: "GET",
+        headers: { "satoken": localStorage.getItem("token") }
+        }).then(response => {
+          console.log(response)
+          let result = response.json()
+          result.then(res => {
+            console.log(res)
+            this.score = res
+            for(var i in this.score)
+            {
+              if(this.score[i][4]==null)
+              {
+                this.score[i][4]="未批改"
+              }
+            }
+          })
+        })
+
+        fetch(this.$URL + "/user/getSign?userId=" +row.id, {
+        method: "GET",
+        headers: { "satoken": localStorage.getItem("token") }
+        }).then(response => {
+          console.log(response)
+          let result = response.json()
+          result.then(res => {
+            console.log(res)
+            this.count=res.count
+          })
+        })
+      //console.log("taskId", this.taskId)
+      console.log(row)
+      this.studentid=row.id
+      // this.markedStudentId = row.id
+      // this.markedStudentScore = row.score
+      this.markModalVisible = !this.markModalVisible
+    },
+    updateScore(){
+      let req = {
+        studentId: this.studentid,
+        courseId: this.courseId,
+        score: this.markedStudentScore
+      }
+      console.log(req)
+      fetch(this.$URL + "/takes/mark", {
+        method: "PUT",
+        body: JSON.stringify(req),
+        headers: {
+          "Content-Type": "application/json",
+          "satoken": localStorage.getItem("token")
+        }
+      }).then(response => {
+        console.log(response)
+        let result = response.json()
+        result.then(res => {
+          console.log(res)
+          if (res.code == 200) {
+            this.$notification.success("成绩上传/更新成功")
+            /**
+             * 以下replace语句是用来跳转到一个空白页面{name: 'Refresh'}实现当前界面自动重新加载的效果
+             * 
+             * 经实践，填path比填name更快。
+             */
+            // this.$router.replace({name: 'Refresh'})
+            this.$router.replace({path: '/refresh'})
+          } else {
+            this.$notification.error("成绩上传失败")
+          }
+        })
+      })
+    },
+    switchToTask (scope) {
       // console.log("调用switchToCourse时，courseId传过去了吗？ courseId的值是：" + id)
       // console.log("调用switchToCourse时，index的值是：" + index)
-      // console.log(this.courses[index])
-      localStorage.setItem("currentTaskId", taskId)
+      // console.log(this.courses[index])‘
+      console.log(scope)
+      localStorage.setItem("currentTaskId", scope.id)
+      // localStorage.setItem("currentTaskId", taskId)
       // console.log("设置courseID了吗？courseId=" + localStorage.getItem("courseId"))
       this.$router.push({
         name: 'GradeManagementTaskPage',
         params: {
-          taskId: this.tasks[index].id,
-          taskName: this.tasks[index].name,
-          courseId: this.courseId,
-          type: this.tasks[index].type,
-          deadline: this.tasks[index].deadline
+          taskId: scope.id,
+          taskName: scope.name,
+          courseId: scope.courseId,
+          type: scope.type,
+          deadline: scope.deadline
         }
       })
     }
--- front-end/src/components/GradeManagement/MyGrades.vue
+++ front-end/src/components/GradeManagement/MyGrades.vue
@@ -6,9 +6,14 @@
             <va-breadcrumbs-item label="我的成绩" to="/home/mygrades" />
         </va-breadcrumbs>
       </div>
-    <va-card>
-      <va-card-title style="font-size: 20px">我的成绩</va-card-title>
-        <va-card-content>
+      <a-button style="margin-right: 10px" @click="this.Scorestatus=1">实验项目成绩</a-button>
+      <a-button @click="this.Scorestatus=2">课程最终成绩</a-button>
+    <va-card v-if="this.Scorestatus==1">
+      <div style="display: flex">
+      </div>
+      <va-card-title style="font-size: 20px">实验项目成绩</va-card-title>
+      <h1 v-if="this.show==1">您尚未参加任何实验项目</h1>
+        <va-card-content v-if="this.show==0">
            
            <div v-for="(item,index) in scorelist" :key="index">
              <h1>{{name[index]}}</h1>
@@ -28,6 +33,60 @@
         </va-card-content>
         
     </va-card>
+ 
+ 
+ <!-- <va-card v-if="this.Scorestatus==2">
+      <div style="display: flex">
+      </div>
+      <va-card-title style="font-size: 20px">课程最终成绩</va-card-title>
+      <h1 v-if="this.show==1">您尚未参加任何实验项目</h1>
+        <va-card-content v-if="this.show==0">
+           
+           <div v-for="(item,index) in name" :key="index">
+             <h1>{{name[index]}}</h1>
+             <br>
+            <el-table :data="scorelist[index]" style="width: 100%">
+              <el-table-column prop="2" label="项目ID"  />
+              <el-table-column prop="3" label="项目名字"  />          
+              <el-table-column prop="coursescore.id" label="得分" />
+             
+            </el-table>
+
+            <h2 v-if="score[index]!=0">{{score[index]}}</h2>
+            <h2 v-else>本项目未参加测试</h2>
+            <br>
+           </div>
+          
+        </va-card-content>
+        
+    </va-card> -->
+    <va-card v-if="this.Scorestatus==2">
+      <div style="display: flex">
+      </div>
+      <va-card-title style="font-size: 20px">课程最终成绩</va-card-title>
+      <h1 v-if="this.show==1">您尚未参加任何实验项目</h1>
+        <va-card-content v-if="1">
+           
+          <el-table v-if="1"
+          :data="coursescore.filter(
+              (data) =>
+                !search || data.name.toLowerCase().includes(search.toLowerCase())
+            )
+          "
+          style="width: 100%"
+          >        
+            <el-table-column label="项目名称" prop="name" />{{name[1]}}
+            <el-table-column label="得分" prop="score" />
+            <el-table-column align="right">
+            <template #header>
+              <el-input v-model="search" size="mini" placeholder="Type to search" />
+            </template>
+            
+              </el-table-column>
+          </el-table>
+        </va-card-content>
+        
+    </va-card>
   </div>
   
 </template>
@@ -36,13 +95,17 @@
 export default {
   data () {
     return {
+      Scorestatus:1,
       showAvatar: false,
       count:0,
       num:0,
       studentId: '',
       scorelist:[],
+      coursename:[],
       name:[],
       score:[], 
+      show:0,
+      coursescore:[],
       // taskID:'',
       // answer:'',
       // finishTime:'',
@@ -61,59 +124,72 @@ export default {
     }).then((res) => {
       var result = res.json()
       result.then((res) => {
-        //console.log(res)
-        this.scorelist=res
-        //console.log(this.scorelist[0][0][1])
-        for(var i in this.scorelist)
+        console.log(res)
+        if(res.code==402)
+          this.show=1
+        else
         {
-          //console.log(this.scorelist[i].length)
-          if(this.scorelist[i].length!=0)
+          this.scorelist=res
+          //console.log(this.scorelist[0][0][1])
+          for(var i in this.scorelist)
           {
-            //this.name[i]=this.scorelist[i][0][1]
-            this.num++
-            //console.log(this.name)
-            for(var x in this.scorelist[i])
+            //console.log(this.scorelist[i].length)
+            if(this.scorelist[i].length!=0)
             {
-              if(this.scorelist[i][x][4]==null)
-                 {
-                   this.count++
-                   this.scorelist[i][x][4]="未批改"
-                   //continue
-                 }
-                 else
-                 {
-              console.log(this.score[i]+" "+this.scorelist[i][x][4])
-              if((x==0)&&(this.scorelist[i][x][4]!="未批改"))
-                this.score[i]=this.scorelist[i][x][4]
+              //this.name[i]=this.scorelist[i][0][1]
+              this.num++
+              //console.log(this.name)
+              for(var x in this.scorelist[i])
+              {
+                if(this.scorelist[i][x][4]==null)
+                  {
+                    this.count++
+                    this.scorelist[i][x][4]="未批改"
+                    //continue
+                  }
+                  else
+                  {
+                console.log(this.score[i]+" "+this.scorelist[i][x][4])
+                if((x==0)&&(this.scorelist[i][x][4]!="未批改"))
+                  this.score[i]=this.scorelist[i][x][4]
+                else
+                  this.score[i]=this.score[i]+this.scorelist[i][x][4]
+                console.log("woshi"+this.score[i])
+              }
+              }
+              console.log(!this.score[i])
+              if(!this.score[i])
+                this.score[i]="全部项目未批改"
               else
-                this.score[i]=this.score[i]+this.scorelist[i][x][4]
-              console.log("woshi"+this.score[i])
-            }
-            }
-            console.log(!this.score[i])
-            if(!this.score[i])
-              this.score[i]="全部项目未批改"
-            else
-              this.score[i]="本项目的平均分为："+this.score[i]/(this.scorelist[i].length- this.count)+"分"
-            
-            // if(!this.score[i])
-            //   this.score[i]="全部项目未批改"
-            this.count=0
-        }
-          else{
-            this.score[i]=0
-            //this.name[i]=[]
+                this.score[i]="本项目的平均分为："+this.score[i]/(this.scorelist[i].length- this.count)+"分"
+              
+              // if(!this.score[i])
+              //   this.score[i]="全部项目未批改"
+              this.count=0
           }
-         
+            else{
+              this.score[i]=0
+              //this.name[i]=[]
+            }
+          
+          }
+          console.log(this.score)
         }
-        //console.log(this.name)
-        // this.name = res.name
-        // this.gender = res.gender
-        // this.email = res.email
-        // this.identity = res.identity
-        // this.activated = res.activated
-        
-        console.log(this.score)
+      }),
+    fetch(this.$URL + "/takes/get/student?studentId=" + this.studentId, {
+    method: "GET",
+    headers: { "satoken": localStorage.getItem("token") }
+    }).then((res) => {
+      var result = res.json()
+      result.then((res) => {
+        //console.log(res)
+        this.coursescore=res
+        // this.coursescore.map(((item)=> {
+        // this.coursescore.push(Object.assign({},item,{name:"1"}))
+        // }))
+        for(var h in this.coursescore)
+          this.coursescore[h].name=this.name[h]
+        console.log(this.coursescore)
       })
     }),
 
@@ -123,36 +199,16 @@ export default {
     }).then((res) =>{
       var result = res.json()
       result.then((res) => {
-      //  console.log(res)
-      // for(var i in res)
-      // {
-      //   console.log(res[i].name)
-      //   if(this.name.indexOf(res[i].name)>-1)
-      //   {
-      //      console.log(res[i].name)
-      //   }
-      //   else
-      //   {
-      //     this.name[this.num]=res[i].name
-      //     this.num++
-      //   }
-
-        
-      // }
       for(var i in res)
       {
         this.name.push(res[i].name)
       }
-      
+       for(var h in this.coursescore)
+          this.coursescore[h].name=this.name[h]
       })
     })
-
-    // localStorage.setItem("gender", this.gender)
-    // console.log("this.gender: " , this.gender)
-    // localStorage.setItem("username", this.name)
-    // console.log("this.username: " , this.name)
-    // console.log("name存入localstorage了吗？" + this.localStorage.getItem("gender"))
-    // console.log("gender存入localstorage了吗？ " + this.localStorage.getItem("username"))
+    })
+  
     
   },
   methods: {
