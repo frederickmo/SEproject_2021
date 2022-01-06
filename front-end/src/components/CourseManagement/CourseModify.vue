e83cfcefa6b70179cd2554781fd5aae1ba20e579
zf909 <80331399+zf909@users.noreply.github.com>
2022-01-06 19:24:06 +0800

    Add files via upload

--- front-end/src/components/CourseManagement/CourseModify.vue
+++ front-end/src/components/CourseManagement/CourseModify.vue
@@ -269,7 +269,26 @@
           <a-button v-if="this.on==0" @click="handleClick" color="#e0e5df" style="color: rgb(40,40,40); ">点击添加教师</a-button>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;    
          
-          <a-button v-if="this.on==0" @click="handleClick1" color="#e0e5df" style="color: rgb(40,40,40); ">点击添加学生</a-button>
+          <a-button v-if="this.on==0" @click="handleClick1" color="#e0e5df" style="color: rgb(40,40,40); ">点击添加学生</a-button> 
+          <div style="height:20px"/>
+          <el-upload  :action="url" :http-request="importExcel" list-type="text" :show-file-list="false" >
+              <el-button size="small"  type="primary">批量导入学生</el-button>
+          </el-upload>
+         <!-- <el-upload
+          ref="upload"
+          :action="getUploadUrl()"
+          :headers="headers()"
+          :data="additionalData()"
+          :auto-upload="false"
+          style="text-align: center"
+          :on-success="handleOnSuccess"
+          :on-error="handleOnError"
+          >
+            <template #trigger>
+              <va-button gradient :rounded="false">选择文件</va-button>
+            </template>
+            <va-button style="margin-left: 20px" flat :rounded="false" @click="submitUpload"> 上传</va-button>
+          </el-upload>  -->
           </div>
           <div style="height: 10px" />
 
@@ -347,9 +366,15 @@
 </template>
 
 <script>
+import XLSX from 'xlsx' 
 export default {
+
+  
+
     data () {
         return {
+          name:'学号',
+          dataForm:[],
           stu:[],
           tea:[],
           removeteaVisible:false,
@@ -436,7 +461,56 @@ export default {
 
     },
   methods: {
-     
+      importExcel (content) {
+    const file = content.file
+    // let file = file.files[0] // 使用传统的input方法需要加上这一步
+    const types = file.name.split('.')[1]
+    const fileType = ['xlsx', 'xlc', 'xlm', 'xls', 'xlt', 'xlw', 'csv'].some(item => item === types)
+    if (!fileType) {
+      this.$message('格式错误！请重新选择')
+      return
+    }
+    this.file2Xce(file).then(tabJson => {
+      if (tabJson && tabJson.length > 0) {
+        this.xlsxJson = tabJson
+        this.fileList = this.xlsxJson[0].sheet
+        
+        this.fileList.forEach((item, index, arr) => {
+          console.log(item,index,arr)
+          // if (item[n] === this.name) {
+          //   console.log(item[n]+"90")
+          //   this.dataForm = item['XXXX'] // 需要的值的表头
+          // }
+            //let n = '学号'
+            this.studentid=item["学号"]
+            this.handleOk1()
+        })
+      }
+    })
+  },
+  file2Xce (file) {
+    return new Promise(function (resolve, reject) {
+      console.log(reject)
+      const reader = new FileReader()
+      reader.onload = function (e) {
+        const data = e.target.result
+        this.wb = XLSX.read(data, {
+          type: 'binary'
+        })
+        const result = []
+        this.wb.SheetNames.forEach((sheetName) => {
+          result.push({
+            sheetName: sheetName,
+            sheet: XLSX.utils.sheet_to_json(this.wb.Sheets[sheetName])
+          })
+        })
+        resolve(result)
+      }
+      // reader.readAsBinaryString(file.raw)
+      reader.readAsBinaryString(file) // 传统input方法
+    })
+  },
+
      handleClick() {
       this.visible = true;
     },
@@ -498,7 +572,7 @@ export default {
     // },
     handleOk1() {
       // this.visible = false;
- let submitForm = {
+            let submitForm = {
                 studentId : this.studentid,
                 courseId: this.courseId,
                 
@@ -521,24 +595,26 @@ export default {
                          
                         //this.$router.go(0)
                          fetch(this.$URL + "/takes/get/course/detail?courseId=" + this.courseId, {
-      method: "GET",
-      headers: { "satoken": localStorage.getItem("token") }
-    }).then(response => {
-      console.log(response)
-      let result = response.json()
-      result.then(res => {
-        console.log(res)
-        this.student = res
-      })
-    })
-                    }
-                    else{
-                      this.$notification.error('输入学生id有误！')
-                        //this.$router.go(-1)
-                    }
+                          method: "GET",
+                          headers: { "satoken": localStorage.getItem("token") }
+                        }).then(response => {
+                          console.log(response)
+                          let result = response.json()
+                          result.then(res => {
+                            console.log(res)
+                            this.student = res
+                          })
+                        })
+                      }
+                      else{
+                        //if(res.msg=="该学生已经选修该实验课程")
+                        console.log(res.msg)
+                        this.$notification.error(res.msg)
+                          //this.$router.go(-1)
+                      }
+              })
                 })
-            })
-            this.studentid=''
+              this.studentid=''
     },
     handleCancel1() {
       this.visible = false;
